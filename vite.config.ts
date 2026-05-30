e927e095226784ea4acf2119beaf21cded17d91d
Ivandv19 <ivanmx19@outlook.com>
2026-05-30 11:59:41 -0600

    chore: configura develop, CodeGraph, wrangler, CI/CD y vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
-	test: {
-		globals: true,
-		environment: "jsdom",
-		setupFiles: "./src/test/setup.ts",
-	},
 });
